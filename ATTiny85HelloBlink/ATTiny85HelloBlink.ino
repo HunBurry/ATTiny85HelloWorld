10164fb6d9b112ba99e513e3eb7d5b5a817d6dc5
Hunter Berry <htberry99@gmail.com>
2021-01-27 13:25:50 -0500

    Create ATTiny85HelloBlink.ino

---
+++ ATTiny85HelloBlink/ATTiny85HelloBlink.ino
@@ -0,0 +1,48 @@
+#include <Bounce2.h>
+
+int mode, light, button;
+boolean state;
+Bounce b = Bounce();
+
+void setup() {
+  mode = 0;
+  state = true;
+  light = 0; ///change this
+  button = 0;
+  
+  b.attach(button, INPUT_PULLUP);
+  b.interval(50);
+
+  pinMode(light, OUTPUT);
+}
+
+void loop() {
+  b.update();
+  if (b.rose()) {
+    changeMode();
+  }
+  activate();
+}
+
+void changeMode() {
+  if (mode == 2) {
+    mode = 0;
+  }
+  else {
+    mode++;
+  }
+}
+
+void activate() {
+  if (mode == 0) {
+    digitalWrite(light, false);
+  }
+  else if (mode == 1) {
+    digitalWrite(light, true);
+  }
+  else if (mode == 2) {
+   state = !state; 
+   digitalWrite(light, state);
+   delayATTiny85HelloBlink(100);
+  } 
+}
